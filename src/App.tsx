88e2247073252dca98453a09f5c7b7e410f87c4c
yimeric7 <112520475+yimeric7@users.noreply.github.com>
2024-10-05 17:14:50 -0500

    draggable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,86 @@
-import { framer, CanvasNode } from "framer-plugin"
-import { useState, useEffect } from "react"
-import "./App.css"
+// App.tsx
+import { framer, Draggable } from "framer-plugin";
+import "./App.css";
 
 framer.showUI({
-    position: "top right",
-    width: 240,
-    height: 95,
-})
-
-function useSelection() {
-    const [selection, setSelection] = useState<CanvasNode[]>([])
-
-    useEffect(() => {
-        return framer.subscribeToSelection(setSelection)
-    }, [])
-
-    return selection
-}
+  position: "top right",
+  width: 240,
+  height: 95,
+  resizable: true,
+});
 
 export function App() {
-    const selection = useSelection()
-    const layer = selection.length === 1 ? "layer" : "layers"
+  const navBarURL =
+    "https://framer.com/m/navbar-Simple-Topbar-0PiY.js@ejyoG8KRkJz02LSus0KU";
 
-    const handleAddSvg = async () => {
-        await framer.addSVG({
-            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="#999" d="M20 0v8h-8L4 0ZM4 8h8l8 8h-8v8l-8-8Z"/></svg>`,
-            name: "Logo.svg",
-        })
-    }
+  const navBarDragData = {
+    type: "componentInstance",
+    url: navBarURL,
+    attributes: {
+      name: "Navbar",
+    },
+  };
 
-    return (
-        <main>
-            <p>
-                Welcome! Check out the{" "}
-                <a href="https://framer.com/developers/plugins/introduction" target="_blank">
-                    Docs
-                </a>{" "}
-                to start. You have {selection.length} {layer} selected.
-            </p>
-            <button className="framer-button-primary" onClick={handleAddSvg}>
-                Insert Logo
-            </button>
-        </main>
-    )
+  const heroURL = "https://framer.com/m/Hero-EzMh.js@Vq7KinjkRjYZLixdCHHM";
+
+  const heroDragData = {
+    type: "componentInstance",
+    url: heroURL,
+    attributes: {
+      name: "Hero",
+    },
+  };
+
+  const footerURL = "https://framer.com/m/Footer-bZ9X.js@5X7H61dUhusmsaqcUx6M";
+
+  const footerDragData = {
+    type: "componentInstance",
+    url: footerURL,
+    attributes: {
+      name: "Footer",
+    },
+  };
+
+  const textStylesURL =
+    "https://framer.com/m/texts-oB98.js@5LVeZBTN6FIWutiBmStz";
+
+  const textStylesDragData = {
+    type: "componentInstance",
+    url: textStylesURL,
+    attributes: {
+      name: "Text Styles",
+    },
+  };
+
+  const colorStylesURL =
+    "https://framer.com/m/color-xeIT.js@YnO3TsADa9zY43FnPLYp";
+
+  const colorStylesDragData = {
+    type: "componentInstance",
+    url: colorStylesURL,
+    attributes: {
+      name: "Color Styles",
+    },
+  };
+
+  return (
+    <main>
+      <h1>Segment UI</h1>
+      <Draggable data={navBarDragData}>
+        <button className="framer-button-primary">Navbar</button>
+      </Draggable>
+      <Draggable data={heroDragData}>
+        <button className="framer-button-primary">Hero</button>
+      </Draggable>
+      <Draggable data={footerDragData}>
+        <button className="framer-button-primary">Footer</button>
+      </Draggable>
+      <Draggable data={textStylesDragData}>
+        <button className="framer-button-primary">Text Styles</button>
+      </Draggable>
+      <Draggable data={colorStylesDragData}>
+        <button className="framer-button-primary">Color Styles</button>
+      </Draggable>
+    </main>
+  );
 }
